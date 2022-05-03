501be62463f3fc573a3f0cb9f3407d3ef7b3fddc
Flamerson De Andrade <68919243+Flamerson@users.noreply.github.com>
2022-05-03 20:36:26 -0300

    rotas

--- src/routes.js
+++ src/routes.js
@@ -41,6 +41,36 @@ routes.get("/album/:album", (req , res) => {
     })
 })
 
+// retorno de usuarios.
+routes.get("/todos/user", (req,res) => {
+    mysql.getConnection((error, conn) =>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM usuarios',
+            (error, result) => {
+                conn.release()
+                if(error){return res.status(500).send({error:error, response:null})}
+                res.status(200).send(result)
+            }
+        )
+    })
+})
+
+routes.get("/admin/users", (req,res) => {
+    mysql.getConnection((error, conn) =>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM usuarios WHERE admin = ?',
+            ["true"],
+            (error, result) => {
+                conn.release()
+                if(error){return res.status(500).send({error: error, response:null})}
+                res.status(200).send(result)
+            }
+        )
+    })
+})
+
 //requisição de todas as musicas , pedindo no geral
 routes.get('/todos' , (req, res) =>{
     mysql.getConnection((error , conn) => {
@@ -100,14 +130,12 @@ routes.get('/audio/:song', async (req, res) =>{
 
 //Post
 // adiciona uma musica ao banco de dados
-routes.post('/teste', multer(multerConfig).single('audio') ,(req, res)=>{
-    console.log(req.file)
-
+routes.post('/cadastrar/musica', multer(multerConfig).single('audio') ,(req, res)=>{
     mysql.getConnection((error, conn) => {
         if(error){return res.status(500).send({error : error})}
         conn.query(
-            'INSERT INTO musicas (musicaid , nome ,banda, src, url, album, genero) VALUES (?,?,?,?,?,?,?)',
-            [req.body.id, req.body.nome, req.body.banda, req.file.key , req.file.location , req.body.album, req.body.genero],
+            'INSERT INTO musicas (nome ,banda, src, url, album, genero) VALUES (?,?,?,?,?,?)',
+            [req.body.nome, req.body.banda, req.file.key , req.file.location , req.body.album, req.body.genero],
             (error) => {
                 conn.release()
                 if(error){
@@ -141,11 +169,129 @@ routes.post('/cadastrar/album', (req,res) => {
     })
 })
 
-//Delete
-
+routes.post('/cadastrar/user', (req, res) => {
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'INSERT INTO usuarios (nome,email,senha) VALUES (?,?,?)',
+            [req.body.nome , req.body.email , req.body.senha],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem:'Usuario inserido com sucesso'})
+            }
+        )
+    })
+})
 
 //Update
 
+routes.put('/atualizar/album/:id', (req,res) => {
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'UPDATE album SET nome=? WHERE id=?',
+            [req.body.nome, req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'Album atualizado com sucesso'})
+            }
+        )
+    })
+})
 
+// update de usuarios , atualizar os dados.
+routes.put('/usuario/atualizar/:id', (req,res) => {
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'UPDATE usuarios SET nome=?, email=?, senha=? WHERE id=?',
+            [req.body.nome,req.body.email,req.body.senha , req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'Usuario Atualizado com sucesso'})
+            }
+        )
+    })
+})
+
+routes.put('/adicionar/admin/:id', (req,res) =>{
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'UPDATE usuarios SET admin=? WHERE id=?',
+            ["true",req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'O usuario agora é um administrador!'})
+            }
+        )
+    })
+})
+
+routes.put('/remover/admin/:id', (req,res) =>{
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'UPDATE usuarios SET admin=? WHERE id=?',
+            ["false",req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'O usuario não é mais um administrador!'})
+            }
+        )
+    })
+})
+
+//Delete
+
+routes.delete('/deletar/musica/:id', (req, res)=> {
+    mysql.getConnection((error , conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'DELETE FROM musicas WHERE musicaid=?',
+            [req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'A musica foi removida com sucesso'})
+            }
+        )
+    })
+})
+
+routes.delete('/deletar/album/:id', (req,res)=>{
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'DELETE FROM album WHERE id=?',
+            [req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'Album removido com sucesso'})
+            }
+        )
+    })
+})
+
+routes.delete('/deletar/usuario/:id', (req,res) =>{
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'DELETE FROM usuarios WHERE id=?',
+            [req.params.id],
+            (error)=>{
+                conn.release()
+                if(error){return res.status(500).send({error:error})}
+                res.status(201).send({mensagem: 'Usuario Removido com sucesso.'})
+            }
+        )
+    })
+})
 
 module.exports = routes; // exporta a nossa rota para onde quiser usar
